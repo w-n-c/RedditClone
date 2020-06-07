1a21db4ccb709b3f97e599ed11275d0f5dbd77ad
Will Newell <newellwm@gmail.com>
2020-06-06 22:39:31 -0400

    Initialize spring boot app

---
+++ src/test/java/codes/newell/app/RedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.newell.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
