6c0a8428312b6db78dd6d98aa41a5ef5982625b0
Will Newell <newellwm@gmail.com>
2020-06-07 09:07:16 -0400

    AuthService constructor injects and creates tokens

--- src/main/java/codes/newell/service/AuthService.java
+++ src/main/java/codes/newell/service/AuthService.java
@@ -1,6 +1,7 @@
 package codes.newell.service;
 
 import java.time.Instant;
+import java.util.UUID;
 
 import javax.transaction.Transactional;
 
@@ -10,29 +11,42 @@ import org.springframework.stereotype.Service;
 
 import codes.newell.dto.RegisterRequest;
 import codes.newell.model.User;
+import codes.newell.model.VerificationToken;
 import codes.newell.repository.UserRepository;
+import codes.newell.repository.VerificationTokenRepository;
 
 @Service
 public class AuthService {
-	
-	private final PasswordEncoder passwordEncoder;	
-	private final UserRepository userRepository;
-	
+
+	private final PasswordEncoder encoder;	
+	private final UserRepository ur;
+	private final VerificationTokenRepository vtr;
+
 	@Autowired
-	public AuthService(PasswordEncoder passwordEncoder, UserRepository userRepository) {
-		this.passwordEncoder = passwordEncoder;
-		this.userRepository = userRepository;
+	public AuthService(PasswordEncoder encoder, UserRepository ur, VerificationTokenRepository vtr) {
+		this.encoder = encoder;
+		this.ur = ur;
+		this.vtr = vtr;
 	}
 
-	
 	@Transactional
 	public void signup(RegisterRequest registerRequest) {
 		User user = new User();
 		user.setPassword(registerRequest.getPassword());
 		user.setEmail(registerRequest.getEmail());
-		user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+		user.setPassword(encoder.encode(registerRequest.getPassword()));
 		user.setCreated(Instant.now());
 		user.setEnabled(false);
-		userRepository.save(user);
+		ur.save(user);
+		generateVerificationToken(user);
+	}
+
+	@Transactional
+	private void generateVerificationToken(User user) {
+		String token = UUID.randomUUID().toString();
+		VerificationToken verifier = new VerificationToken();
+		verifier.setToken(token);
+		verifier.setUser(user);
+		vtr.save(verifier);
 	}
 }
