47962f591e8d737f1df5c99e7e9b333d0b6c7937
Will Newell <newellwm@gmail.com>
2020-06-08 09:45:00 -0400

    Add partial implementation of JwtProvider

---
+++ src/main/java/codes/newell/security/JwtProvider.java
@@ -0,0 +1,52 @@
+package codes.newell.security;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import codes.newell.exceptions.SpringRedditException;
+import codes.newell.model.User;
+import io.jsonwebtoken.Jwts;
+
+@Service
+public class JwtProvider {
+	
+	private KeyStore keyStore;
+	
+    @PostConstruct
+    public void init() {
+        try {
+            keyStore = KeyStore.getInstance("JKS");
+            InputStream resourceAsStream = getClass().getResourceAsStream("/springblog.jks");
+            keyStore.load(resourceAsStream, "secret".toCharArray());
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+            throw new SpringRedditException("Exception occurred while loading keystore", e);
+        }
+    }
+	
+	public String generateToken(Authentication auth) {
+		User principal = (User)auth.getPrincipal();
+		return Jwts.builder()
+				.setSubject(principal.getUsername())
+				.signWith(getPrivateKey())
+				.compact();
+	}
+
+    private PrivateKey getPrivateKey() {
+        try {
+            return (PrivateKey) keyStore.getKey("springblog", "secret".toCharArray());
+        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+            throw new SpringRedditException("Exception occured while retrieving public key from keystore", e);
+        }
+    }
+}
