ac38430a4af4c1175f8432b1edac6bd568a181b5
Will Newell <newellwm@gmail.com>
2020-06-07 21:12:14 -0400

    Add UserDetailsService config and implementation

--- src/main/java/codes/newell/config/SecurityConfig.java
+++ src/main/java/codes/newell/config/SecurityConfig.java
@@ -1,14 +1,23 @@
 package codes.newell.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableWebSecurity
+import lombok.AllArgsConstructor;
+
+@EnableWebSecurity //contains @configuration
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final UserDetailsService uds;
+	
 	@Override
 	public void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.csrf().disable()
@@ -17,6 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.permitAll()
 			.anyRequest()
 			.authenticated();
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder abm) throws Exception {
+		abm.userDetailsService(uds).passwordEncoder(passwordEncoder());
 	}
 	
 	@Bean
---
+++ src/main/java/codes/newell/service/UserDetailsServiceImpl.java
@@ -0,0 +1,46 @@
+package codes.newell.service;
+
+import static java.util.Collections.singletonList;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import codes.newell.model.User;
+import codes.newell.repository.UserRepository;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	private final UserRepository ur;
+
+	@Override
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<User> o = ur.findByUsername(username);
+		User user = o.orElseThrow(() -> new UsernameNotFoundException("No user found with username" + username));
+		return new org.springframework.security.core.userdetails.User(
+			user.getUsername(),
+			user.getPassword(),
+			user.isEnabled(),
+			true,
+			true,
+			true,
+			getAuthorities("USER")
+		);
+	}
+	
+	private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+		return singletonList(new SimpleGrantedAuthority(role));
+	}
+
+}
